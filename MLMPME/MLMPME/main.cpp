e5cd0ddb45be5d4b3b625831a21a1b8c354e8937
TaoMiner <god13dog@163.com>
2017-03-15 15:34:22 -0400

    init

---
+++ MLMPME/MLMPME/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MLMPME
+//
+//  Created by 曹艺馨 on 17/3/15.
+//  Copyright © 2017年 ethan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
