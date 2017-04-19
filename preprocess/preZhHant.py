b307e0324fb2fb0dc6d4b2344b71b32a74252b40
TaoMiner <god13dog@163.com>
2017-04-19 11:05:37 -0400

    add zh vocal entity

--- preprocess/preZhHant.py
+++ preprocess/preZhHant.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
-
 import codecs
 import re
+import HTMLParser
+from itertools import izip
+import string
+import jieba
+
+htmlparser = HTMLParser.HTMLParser()
 
 
 # parameters
@@ -20,44 +25,96 @@ class options():
 
         self.wiki_cleaned_file = self.dump_path + self.output_path + '/wiki_article_cl.xml'
         self.title_file = self.dump_path + self.output_path + '/wiki_article_title'
+        self.raw_entity_file = self.dump_path + self.output_path + '/raw_vocab_entity.dat'
+        # the simplified chinese wiki anchor file from wiki_anchor_text extracted by WikiExtractor
+        self.raw_anchor_file = self.dump_path + self.output_path + '/wiki_anchor_chs'
+
         self.redirect_file = self.dump_path + self.output_path + '/vocab_redirects.dat'
         self.raw_vocab_entity_file = self.dump_path + self.output_path + '/vocab_entity_all.dat'
         self.mono_outlink_file = self.dump_path + self.output_path + '/mono_kg.dat'
         self.vocab_entity_file = self.dump_path + self.output_path + '/vocab_entity.dat'
         self.cross_link_file = self.dump_path + self.output_path + '/cross_links.dat'
-        self.raw_anchor_file = self.dump_path + self.output_path + '/wiki_anchor_text'
         self.anchor_file = self.dump_path + self.output_path + '/anchor_text_cl.dat'
         self.mention_file = self.dump_path + self.output_path + '/mention_count.dat'
 
-nsidRE = re.compile(r'(\d{1,}):(\d{1,}):(.*)')
+class ZhPreprocessor():
+    def __init__(self):
+        self.nsidRE = re.compile(r'(\d{1,}):(\d{1,}):(.*)')
+        self.formatRE = re.compile(r'-\{.*?(zh-hans|zh-cn):(?P<label>[^;]*?)([;].*?\}|\})-')
+        self.total_entity_id = {}
+        self.total_id_entity = {}
+        self.entity_id = {}
+        self.id_entity = {}
 
-titles = set()
+    # format zh-cn and zh-tw
+    def formatRawWiki(self, zhwiki_file, zhwiki_format_file):
+        line_count = 0
+        with codecs.open(zhwiki_file, 'r') as fin:
+            with codecs.open(zhwiki_format_file, 'w', 'utf-8') as fout:
+                for line in fin:
+                    line_count += 1
+                    if line_count % 1000000 == 0: print "has processed %d lines!" % line_count
+                    line = line.decode('utf-8', 'ignore')
+                    line = self.formatRE.sub('\g<label>', line)
+                    fout.write(line)
 
-def loadTitleIndex(filename):
-    with codecs.open(filename, 'r', 'utf-8') as fin:
-        for line in fin:
-            m = nsidRE.match(line.strip())
-            if m != None:
-                id = m.group(2)
-                title = m.group(3)
-                if title in titles:
-                    print title.encode('utf-8')
-                else:
-                    titles.add(title)
-
-formatRE = re.compile(r'-\{.*?(zh-hans|zh-cn):(?P<label>[^;]*?)(;.*?\}|\})-')
-# format zh-cn and zh-tw
-def zhwikiReplacefj(zhwiki_file, zhwiki_format_file):
-    line_count = 0
-    with codecs.open(zhwiki_file, 'r') as fin:
-        with codecs.open(zhwiki_format_file, 'w', 'utf-8') as fout:
+    def loadTitleIndex(self, filename):
+        with codecs.open(filename, 'r', 'utf-8') as fin:
             for line in fin:
-                line_count += 1
-                if line_count % 1000000 == 0: print "has processed %d lines!" % line_count
+                m = self.nsidRE.match(line.strip())
+                if m != None:
+                    id = m.group(2)
+                    title = m.group(3)
+                    self.total_entity_id[title] = id
+                    self.total_id_entity[id] = title
+        print "successfully load %d title index!" % len(self.total_entity_id)
+
+    def buildEntityDic(self, filename):
+        with codecs.open(filename, 'r', 'utf-8') as fin:
+            for line in fin:
+                title = line.strip()
+                if title in self.total_entity_id:
+                    self.entity_id[title] = self.total_entity_id[title]
+                    self.id_entity[self.total_entity_id[title]] = title
+        print "successfully build %d entities!" % len(self.entity_id)
+
+    def saveEntityDic(self, filename):
+        with codecs.open(filename, 'w', 'utf-8') as fout:
+            for ent in self.entity_id:
+                fout.write('%s\t%s\n' % (htmlparser.unescape(self.entity_id[ent]), htmlparser.unescape(ent)))
+
+    def parseRedirects(self, filename):
+        with codecs.open(filename, 'rb') as fin:
+            for line in fin:
                 line = line.decode('utf-8', 'ignore')
-                line = formatRE.sub('\g<label>', line)
-                fout.write(line)
+                line = line.replace('INSERT INTO `redirect` VALUES (', '')
+                for i in line.strip().split('),('):
+                    m = self.redirectRE.match(i)  # Only select namespace 0 (Main/Article) pages
+                    if m != None:
+                        rd_id = m.group(1)
+                        rd_title = m.group(2)
+                        rd_title = rd_title.replace('\\', '')
+                        rd_title = rd_title.replace('_', ' ')
 
-op = options('zhwiki')
+                        if rd_id not in self.tmp_id_entity or rd_title not in self.entity_id or self.tmp_id_entity[rd_id] == rd_title:
+                            continue
+                        self.id_redirects[rd_id] = self.tmp_id_entity[rd_id]
+                        self.redirects_id[self.tmp_id_entity[rd_id]] = rd_id
+                        self.redirects[self.tmp_id_entity[rd_id]] = rd_title
+                        # remove redirect title in entity dic
+                        if rd_id in self.id_entity:
+                            del self.entity_id[self.id_entity[rd_id]]
+                            del self.id_entity[rd_id]
+        print "successfully parse %d redirects for %d entities!" % (len(self.redirects), len(self.entity_id))
+        del self.tmp_id_entity
+        del self.tmp_entity_id
 
-zhwikiReplacefj(op.wiki_dump, op.wiki_cleaned_file)
+if __name__ == '__main__':
+    op = options('zhwiki')
+    zhPre = ZhPreprocessor()
+    # replace -{zh-cn:xxx;zh-tw:xxx;}-
+    # zhPre.formatRawWiki(op.wiki_dump, op.wiki_cleaned_file)
+    # opencc: convert wiki_cleaned_file to simplified chinese
+    zhPre.loadTitleIndex(op.entity_index_dump)
+    zhPre.buildEntityDic(op.title_file)
+    zhPre.saveEntityDic(op.raw_entity_file)
