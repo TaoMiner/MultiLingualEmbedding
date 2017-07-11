d33011d366ebb03b851ac010179a43f932c0898b
TaoMiner <god13dog@163.com>
2017-07-11 22:11:54 +0800

    add segment

--- preprocess/tt.py
+++ preprocess/tt.py
@@ -1,6 +1,8 @@
 import codecs
 import re
 from preprocess import cleaner
+import jieba
+jieba.set_dictionary('/home/caoyx/data/dict.txt.big')
 
 file = '/home/caoyx/data/paradata/cross_links_labels.en_zh_chs'
 output_file = '/home/caoyx/data/paradata/cross_links_labels.en_zh'
@@ -11,5 +13,8 @@ with codecs.open(file, 'r', 'utf8') as fin:
             items = re.split(r'\t', line.strip())
             if len(items) < 2: continue
             en_str = cleaner.regularize(items[0], 'en')
-            zh_str = cleaner.regularize(items[1], 'zh')
+            seg_list = jieba.cut(items[1], cut_all=False)
+            # some chinese entities contain whitespace
+            seg_line = " ".join(seg_list)
+            zh_str = cleaner.regularize(seg_line, 'zh')
             fout.write("%s\t%s\n" % (en_str, zh_str))
