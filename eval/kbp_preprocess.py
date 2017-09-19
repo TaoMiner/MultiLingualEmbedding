a8a5313e38424beb8fd4c05c7761e86566732a06
TaoMiner <god13dog@163.com>
2017-09-19 10:13:10 +0800

    use ref kb

--- eval/kbp_preprocess.py
+++ eval/kbp_preprocess.py
@@ -11,14 +11,17 @@ testfile14 = kbp_eval_path + '2014/eval/tac_kbp_2014_english_EDL_evaluation_KB_l
 
 ids_file = '/home/caoyx/data/kbp/eval_ids'
 title_file = '/home/caoyx/data/kbp/id_title'
+output_file = '/home/caoyx/data/kbp/id.key'
 
-ref_kb_file = '/home/caoyx/data/kbp/freebase_links_en.ttl'
+freebase_file = '/home/caoyx/data/kbp/freebase_links_en.ttl'
+ref_kb_path = '/home/caoyx/data/kbp/LDC2015E42_TAC_KBP_Knowledge_Base_II-BaseKB/data/'
 
 def extractIds(file, id_set):
     with codecs.open(file, 'r', encoding='UTF-8') as fin:
         for line in fin:
             items = re.split(r'\t', line)
             if len(items) < 5: continue
+            if items[4].startswith('NIL') : continue
             id_set.add(items[4])
     print len(id_set)
 
@@ -45,6 +48,27 @@ def extractTitle(file, id_set, id_map):
                     tmp_list.append(title)
                     id_map[id] = tmp_list
 
+label_re = re.compile(r'<http://rdf.basekb.com/ns/(.*?)>')
+def extractTitle2(path, id_set, id_map):
+    if os.path.isdir(path):
+        for root, dirs, list in os.walk(path):
+            for l in list:
+                if l.startswith('.') or (not l.startswith('label-m-') and not l.startswith('webpages-m-')): continue
+                print "processing file: %s ..." % l
+                with gzip.open(os.path.join(ref_kb_path, l), 'r') as fin:
+                    for line in fin:
+                        line = line.decode('utf8')
+                        items = re.split(r'\t', line.strip())
+                        if len(items) < 3: continue
+                        m = label_re.match(items[0])
+                        if m != None:
+                            kbid = m.group(1)
+                            if kbid not in id_set: continue
+                            tmp_list = [] if kbid not in id_map else id_map[kbid]
+                            tmp_list.append(items[2])
+                            id_map[kbid] = tmp_list
+    print "extracted %d ids!" % len(id_map)
+
 id_set = set()
 id_map = {}
 extractIds(testfile15, id_set)
@@ -53,36 +77,8 @@ print "extracted %d different entities!" % len(id_set)
 with codecs.open(ids_file, 'w', encoding='UTF-8') as fout:
     for id in id_set:
         fout.write("%s\n" % id)
-extractTitle(ref_kb_file, id_set, id_map)
+extractTitle2(ref_kb_path, id_set, id_map)
 print "extracted titles for %d entities!" % len(id_map)
-with codecs.open(title_file, 'w', encoding='UTF-8') as fout:
-    for id in id_map:
-        fout.write("%s\t%s\n" % (id, '\t'.join(id_map[id])))
-'''
-id_map = {}
-output_file = '/home/caoyx/data/kbp/LDC2015E42_TAC_KBP_Knowledge_Base_II-BaseKB/id.key'
-label_re = re.compile(r'<http://rdf.basekb.com/ns/(.*?)>')
-count = 0
-if os.path.isdir(ref_kb_path):
-    for root, dirs, list in os.walk(ref_kb_path):
-        for l in list:
-            if l.startswith('.') or (not l.startswith('label-m-') and not l.startswith('webpages-m-')) : continue
-            count += 1
-            print "processing file: %s ..." % l
-            with gzip.open(os.path.join(ref_kb_path, l), 'r') as fin:
-                for line in fin:
-                    line = line.decode('utf8')
-                    items = re.split(r'\t', line.strip())
-                    if len(items) < 3 : continue
-                    m = label_re.match(items[0])
-                    if m != None:
-                        kbid = m.group(1)
-                        if kbid not in id_set: continue
-                        tmp_list = [] if kbid not in id_map else id_map[kbid]
-                        tmp_list.append(items[2])
-                        id_map[kbid] = tmp_list
-print "extracted %d ids!" % len(id_map)
 with codecs.open(output_file, 'w', encoding='UTF-8') as fout:
     for id in id_map:
         fout.write("%s\t%s\n" % (id.encode('utf8'), '\t'.join(id_map[id]).encode('utf8')))
-'''
